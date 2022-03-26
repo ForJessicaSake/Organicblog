ff2dbf242c07e800839961dbd20c50f3bd76ec05
Jessica Joseph <jessicajoseph2019@yahoo.com>
2022-03-26 20:40:20 +0100

    this is a work in progress

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-/*============================== SCROLL REVEAL ANIMATION =================================*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2200,
-    delay: 10,
-    // reset: true
-});
+// /*============================== SCROLL REVEAL ANIMATION =================================*/
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '60px',
+//     duration: 2200,
+//     delay: 10,
+//     // reset: true
+// });
 
-sr.reveal('.text,  .latest-blog, .blog-text, .blogp-text, .recipes-text', {delay: 800});
-sr.reveal('.about', { interval: 300 });
+// sr.reveal('.text,  .latest-blog, .blog-text, .blogp-text, .recipes-text', {delay: 800});
+// sr.reveal('.about', { interval: 300 });
